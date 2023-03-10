a6906b384bfb7dedc2d07383089ed5ea24eba9fb
EmilianoChofis <20213tn128@utez.edu.mx>
2023-03-10 14:59:30 -0600

    Implemented Formik at DataForm component in Profile page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -5,7 +5,7 @@ import Data from "bootstrap/js/src/dom/data";
 import {DataForm} from "../components/Profile/DataForm";
 import {PrimaryButton} from "../components/shared/PrimaryButton";
 import {useState} from "react";
-
+import styles from "../assets/css/components/Profile/Profile.module.css"
 
 
 export const Profile = () => {
@@ -31,7 +31,7 @@ export const Profile = () => {
                 </Col>
             </Row>
             <Banner/>
-            <h2 style={{textAlign: "center", marginTop: 40, marginBottom:40}}>Nombre completo</h2>
+            <h2 className={`${styles.titleName}`}>Nombre completo</h2>
             <DataForm edit={show}/>
 
         </>
--- src/components/Profile/DataForm.js
+++ src/components/Profile/DataForm.js
@@ -1,86 +1,127 @@
-import {Button, Col, Form, FormControl, Modal, Row} from "react-bootstrap";
+import {Button, Col, Form as FormBt, Row} from "react-bootstrap";
 import styles from "../../assets/css/components/Profile/Form.module.css";
 import {
     MdVpnKey
 } from 'react-icons/md';
 import {useState} from "react";
 import {PasswordModal} from "./PasswordModal";
+import {TextInput} from "../Form/TextInput";
+import {Formik, Form} from "formik";
+import * as yup from 'yup';
 
 
 export const DataForm = (props) => {
-
+    const nombre = "Emi"
     const {edit} = props;
-    console.log("edit"+edit)
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
-
-    //const handleClose = () => setShow(false);
     const handleShow = () => {
         setShow(true)
-        console.log("shoe"+show)
     }
+
+    const handleOnSubmit = (values, resetForm) => {
+       // login(values.email, values.password);
+    };
+    //formik
+    const objectSchema = yup.object({
+        name: yup.string().required("No puede quedar vacío"),
+        lastname: yup.string().required("No puede quedar vacío"),
+        surname: yup.string().required("No puede quedar vacío"),
+        phone: yup.number().required("El numero de telefono es requerido"),
+        email: yup.string().email().required("No puede quedar vacío")
+    })
 
     return (
         <>
-            <Form>
-                <Row>
-                    <Col>
-                        <Form.Group controlId="formName">
-                            <Form.Label>Nombre</Form.Label>
-                            <FormControl type="text" style={{backgroundColor: "#F3F1F1"}} disabled={!edit}
-                                         value={edit ? null : "nombre"}/>
-                        </Form.Group>
-                    </Col>
+            <Formik
+                initialValues={{
+                    name: `${nombre}`,
+                    lastname: '',
+                    surname: '',
+                    phone: 0,
+                    email: ''
 
-                    <Col>
-                        <Form.Group controlId="formLastName">
-                            <Form.Label>Apellido paterno</Form.Label>
-                            <FormControl type="text" className={`${styles.input}`} disabled={!edit}
-                                         value={edit ? null : "nombre"}/>
-                        </Form.Group>
-                    </Col>
+                }}
+                validationSchema={objectSchema}
+                onSubmit={(values, { resetForm }) => {
+                    handleOnSubmit(values, resetForm);
+                }}
+            >
+                {({errors, values, touched}) => (
+                    <Form>
+                        <Row>
+                            <Col>
+                                <FormBt.Group>
+                                    <TextInput label='Nombre' name='name' style={{backgroundColor: "#F3F1F1"}}
+                                               className={`${styles.input}`} isInvalid={
+                                        !!errors.name && touched.name
+                                    } disabled={!edit}
+                                    />
+                                </FormBt.Group>
+                            </Col>
+                            <Col>
+                                <FormBt.Group>
+                                    <TextInput label='Apellido paterno' name='lastname'
+                                               style={{backgroundColor: "#F3F1F1"}}
+                                               className={`${styles.input}`} isInvalid={
+                                        !!errors.lastname && touched.lastname
+                                    } disabled={!edit}
+                                    />
+                                </FormBt.Group>
+                            </Col>
+                            <Col>
+                                <FormBt.Group>
+                                    <TextInput label='Apellido materno' name='surname'
+                                               style={{backgroundColor: "#F3F1F1"}}
+                                               className={`${styles.input}`} isInvalid={
+                                        !!errors.surname && touched.surname
+                                    } disabled={!edit}
+                                    />
+                                </FormBt.Group>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <FormBt.Group>
+                                    <TextInput label='Telefono' name='phone' style={{backgroundColor: "#F3F1F1"}}
+                                               className={`${styles.input}`} isInvalid={
+                                        !!errors.phone && touched.phone
+                                    } disabled={!edit}
+                                    />
+                                </FormBt.Group>
+                            </Col>
+                            <Col>
+                                <FormBt.Group>
+                                    <TextInput label='Correo electronico' name='email'
+                                               style={{backgroundColor: "#F3F1F1"}}
+                                               className={`${styles.input}`} isInvalid={
+                                        !!errors.email && touched.email
+                                    } disabled={!edit}
+                                    />
+                                </FormBt.Group>
+                            </Col>
+                            <Col>
+                                <button type="button" className={`${styles.btn}`} onClick={handleShow}>
+                                    <MdVpnKey className={`${styles.btnIcon}`}/> Cambiar contraseña
+                                </button>
+                            </Col>
+                        </Row>
+                        <Button hidden={!edit} type={"submit"} disabled={
+                            !values.name || !!errors.name ||
+                            !values.lastname || !!errors.lastname ||
+                            !values.surname || !!errors.surname ||
+                            !values.phone || !!errors.phone ||
+                            !values.email || !!errors.email
+                         }>
+                            Guardar
+                        </Button>
+                    </Form>
+                )}
 
-                    <Col>
-                        <Form.Group controlId="formSurName">
-                            <Form.Label>Apellido materno</Form.Label>
-                            <FormControl type="text" className={`${styles.input}`} disabled={!edit}
-                                         value={edit ? null : "nombre"}/>
-                        </Form.Group>
-                    </Col>
-                </Row>
-
-                <Row>
-                    <Col>
-                        <Form.Group controlId="formPhone">
-                            <Form.Label>Teléfono</Form.Label>
-                            <FormControl type="number" className={`${styles.input}`} disabled={!edit}
-                                         value={edit ? null : "nombre"}/>
-                        </Form.Group>
-                    </Col>
-
-                    <Col>
-                        <Form.Group controlId="formEmail">
-                            <Form.Label>Correo electrónico</Form.Label>
-                            <FormControl type="email" className={`${styles.input}`} disabled={!edit}
-                                         value={edit ? null : "nombre"}/>
-                        </Form.Group>
-                    </Col>
-
-                    <Col>
-                        <button type="button" className={`${styles.btn}`} onClick={handleShow}>
-                            <MdVpnKey className={`${styles.btnIcon}`}/> Cambiar contraseña
-                        </button>
-                    </Col>
-                </Row>
-                <Button hidden={!edit} type={"submit"}>
-                    Guardar
-                </Button>
-
-            </Form>
+            </Formik>
             {/*test*/}
             <PasswordModal show={show} setShow={setShow} handleClose={handleClose}/>
-
 
 
         </>
